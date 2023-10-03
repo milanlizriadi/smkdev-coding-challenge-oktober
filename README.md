d5b66176186d4c56d89e5a0568296127d2482c5e
milanlizriadi <milanlizriadi@gmail.com>
2023-10-03 19:19:00 +0700

    SMKDEV Coding Challenge

---
+++ README.md
@@ -0,0 +1,3 @@
+# SMKDEV Coding Challenge - Oktober
+
+check <a href="index.js">index.js</a> for code
---
+++ index.js
@@ -0,0 +1,7 @@
+var input_number = 6;
+
+for (var i = 1; i <= input_number; i++) {
+  var cube = i * i * i;
+  
+  console.log("Current Number is: " + i + " and the cube is " + cube);
+}
